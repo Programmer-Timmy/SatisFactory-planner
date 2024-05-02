f6c652837ca1f356e20fec59b0fab182e29d8803
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-05-02 11:50:33 +0000

    navbar update

--- private/views/templates/navbar.php
+++ private/views/templates/navbar.php
@@ -1,14 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home">Satisfactroy Planner</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="home">Home</a>
-            </li>
-            <!-- Add more navigation links as needed -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="home">Home</a>
+        </li>
+      </ul>
+      <div class="d-flex">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="btn btn-success" aria-current="page" target="_blank" href="https://forms.gle/fAd5LrGRATYwFHzr7">Leave Feedback</a>
+          </li>
         </ul>
+      </div>
     </div>
+  </div>
 </nav>
-
