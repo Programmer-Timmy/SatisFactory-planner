f1dd91763f1ca7adaddc5309bd3cf1ec605f05a2
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-05-03 07:32:37 +0000

    added icon

--- private/views/templates/header.php
+++ private/views/templates/header.php
@@ -23,6 +23,9 @@ function getPageTitle() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- icon -->
+    <link rel="icon" type="image/x-icon" href="image/icon.jpg">
+    <!-- title -->
     <title><?php echo getPageTitle(); ?></title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
