0a80d1a525398cba1c62d7590931e1f1916b0908
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-05-03 20:24:22 +0200

    added the new functions

--- private/views/pages/maintenance.php
+++ private/views/pages/maintenance.php
@@ -1,4 +1,3 @@
-<?php var_dump($_SERVER); ?>
 <div class="container text-center mt-5">
     <h1 class="display-4">Under Maintenance</h1>
     <p class="lead">We're performing some scheduled maintenance.</p>
