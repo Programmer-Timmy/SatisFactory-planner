679365e7500833e7aa9ad308c2dbec8ce33b8e8e
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-05-03 22:02:44 +0200

    quiqfix

--- private/controllers/GameSaves.php
+++ private/controllers/GameSaves.php
@@ -2,22 +2,42 @@
 
 class GameSaves
 {
+    /**
+     * @param int $owner_id
+     * @return mixed
+     */
     public static function getSaveGameByOwner(int $owner_id)
     {
         return Database::getAll("game_saves", ['*'], [], ['owner_id' => $owner_id]);
     }
 
+    /**
+     * @param int $user_id
+     * @return mixed
+     */
     public static function  getSaveGamesByUser(int $user_id)
 
     {
         return Database::getAll("users_has_game_saves", ['game_saves_id', 'title', 'created_at', 'username as Owner', 'image', 'game_saves.id', 'game_saves.owner_id'], ['game_saves' => 'game_saves.id = users_has_game_saves.game_saves_id', 'users' => 'users.id = game_saves.owner_id'], ['users_has_game_saves.users_id' => $user_id]);
     }
 
+    /**
+     * @param int $id
+     * @return mixed
+     */
     public static function getSaveGameById(int $id)
     {
         return Database::get("users_has_game_saves", ['game_saves.*', 'users.username'], ['game_saves' => 'game_saves.id = users_has_game_saves.game_saves_id', "users" => "game_saves.owner_id = users.id"] ,['game_saves.id' => $id]);
     }
 
+    /**
+     * @param int $user_id
+     * @param string $title
+     * @param array $image
+     * @param array $allowedUsers
+     * @return null
+     * @throws ErrorException
+     */
     public static function createSaveGame(int $user_id, string $title, array $image, array $allowedUsers)
     {
         $createdAt = date('Y-m-d H:i:s');
@@ -35,6 +55,12 @@ class GameSaves
         return $id;
     }
 
+    /**
+     * @param int $user_id
+     * @param int $game_save_id
+     * @return null
+     * @throws ErrorException
+     */
     public static function addUserToSaveGame(int $user_id, int $game_save_id)
     {
 
@@ -42,11 +68,29 @@ class GameSaves
 
     }
 
+    /**
+     * @param int $game_save_id
+     * @param int $biomassBurner
+     * @param int $coalGenerator
+     * @param int $fuelGenerator
+     * @param int $nuclearReactor
+     * @param int $power
+     * @return null
+     */
     public static function updatePowerProduction(int $game_save_id, int $biomassBurner, int $coalGenerator, int $fuelGenerator, int $nuclearReactor, int $power)
     {
         return Database::update("game_saves", ['biomass_burner', 'coal_generator', 'fuel_generator', 'nuclear_power_plant', 'total_power_production'], [$biomassBurner, $coalGenerator, $fuelGenerator, $nuclearReactor, $power], ['id' => $game_save_id]);
     }
 
+    /**
+     * @param int $game_save_id
+     * @param int $owner_id
+     * @param string $title
+     * @param array $image
+     * @param array $allowedUsers
+     * @return bool
+     * @throws ErrorException
+     */
     public static function updateSaveGame(int $game_save_id, int $owner_id, string $title, array $image, array $allowedUsers)
     {
         if ($owner_id != $_SESSION['userId']) {
@@ -63,6 +107,10 @@ class GameSaves
         return true;
     }
 
+    /**
+     * @param int $game_save_id
+     * @return false|void
+     */
     public static function deleteSaveGame(int $game_save_id)
     {
         if (Database::get("game_saves", ['id'], [], ['id' => $game_save_id, 'owner_id' => $_SESSION['userId']]) == false) {
@@ -74,6 +122,11 @@ class GameSaves
         Database::delete("game_saves", ['id' => $game_save_id, 'owner_id' => $_SESSION['userId']]);
     }
 
+    /**
+     * @param int $game_save_id
+     * @param array $image
+     * @return false|null
+     */
     private static function uploadImage(int $game_save_id, array $image)
     {
         self::deleteImage($game_save_id);
@@ -84,6 +137,10 @@ class GameSaves
         return Database::update("game_saves", ['image'], [$game_save_id . '.' . pathinfo($image['name'], PATHINFO_EXTENSION)], ['id' => $game_save_id]);
     }
 
+    /**
+     * @param int $game_save_id
+     * @return void
+     */
     private static function deleteImage(int $game_save_id)
     {
         $gameSave = self::getSaveGameById($game_save_id);
@@ -92,6 +149,10 @@ class GameSaves
         }
     }
 
+    /**
+     * @param int $gameSaveId
+     * @return array
+     */
     public static function getAllowedUsers(int $gameSaveId): array
     {
         $allowedUsers = Database::getAll("users_has_game_saves", ['users_id'], [], ['game_saves_id' => $gameSaveId]);
@@ -102,6 +163,10 @@ class GameSaves
         return $users;
     }
 
+    /**
+     * @param int $userId
+     * @return void
+     */
     public static function transferSaveGames(int $userId)
     {
         $gamesaves = self::getSaveGamesByUser($userId);
