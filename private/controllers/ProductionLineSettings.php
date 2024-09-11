d3ba0a7de02c69c6575802e80002c52a67437a34
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-09-11 09:05:46 +0200

    fixed issues whit deleting

--- private/controllers/ProductionLineSettings.php
+++ private/controllers/ProductionLineSettings.php
@@ -5,29 +5,29 @@ class ProductionLineSettings
 
     public static function getProductionLineSettings(int $productionLineId, int $userId)
     {
-        return Database::get("productionLineSettings", ['*'], [], ['production_line_id' => $productionLineId, 'user_id' => $userId]);
+        return Database::get("productionlinesettings", ['*'], [], ['production_line_id' => $productionLineId, 'user_id' => $userId]);
     }
 
     public static function addProductionLineSettings(int $productionLineId, int $userId)
     {
-        Database::insert("productionLineSettings", ['production_line_id', 'user_id'], [$productionLineId, $userId]);
+        Database::insert("productionlinesettings", ['production_line_id', 'user_id'], [$productionLineId, $userId]);
         return Database::get("productionLineSettings", ['*'], [], ['production_line_id' => $productionLineId, 'user_id' => $userId]);
     }
 
     public static function updateProductionLineSettings(int $productionLineId, int $userId, bool $autoImportExport, bool $autoPowerMachine, bool $autoSave)
     {
-        Database::update("productionLineSettings", ['auto_import_export', 'auto_power_machine', 'auto_save'], [$autoImportExport ? 1 : 0, $autoPowerMachine ? 1 : 0, $autoSave ? 1 : 0], ['production_line_id' => $productionLineId, 'user_id' => $userId]);
-        return Database::get("productionLineSettings", ['*'], [], ['production_line_id' => $productionLineId, 'user_id' => $userId]);
+        Database::update("productionlinesettings", ['auto_import_export', 'auto_power_machine', 'auto_save'], [$autoImportExport ? 1 : 0, $autoPowerMachine ? 1 : 0, $autoSave ? 1 : 0], ['production_line_id' => $productionLineId, 'user_id' => $userId]);
+        return Database::get("productionlinesettings", ['*'], [], ['production_line_id' => $productionLineId, 'user_id' => $userId]);
     }
 
     public static function deleteProductionLineSettings(int $productionLineId)
     {
-        return Database::delete("productionLineSettings", ['production_line_id' => $productionLineId]);
+        return Database::delete("productionlinesettings", ['production_line_id' => $productionLineId]);
     }
 
     public static function deleteProductionLineSettingsByUser(int $userId)
     {
-        return Database::delete("productionLineSettings", ['user_id' => $userId]);
+        return Database::delete("productionlinesettings", ['user_id' => $userId]);
     }
 
 
