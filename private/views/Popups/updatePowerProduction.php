77a0497484221199f57030a46fbdd0c5e2f244a8
Programmer-Timmy <tim.vanderkloet@gmail.com>
2024-09-17 08:51:15 +0200

    UI improvments

--- private/views/Popups/updatePowerProduction.php
+++ private/views/Popups/updatePowerProduction.php
@@ -16,16 +16,16 @@ $powerProduction = PowerProduction::getPowerProduction($gameSave->id);
             <form method="post">
                 <div class="modal-body">
                     <div class="row px-2 pb-1">
-                        <div class="col-6">
+                        <div class="col-5 col-lg-6 ps-3 pe-1">
                             <label for="building">Building</label>
                         </div>
-                        <div class="col-2">
+                        <div class="col-2 px-1">
                             <label for="amount">Amount</label>
                         </div>
-                        <div class="col-2">
+                        <div class="col-3 col-lg-2 px-1">
                             <label for="clock_speed">Clock Speed</label>
                         </div>
-                        <div class="col-2">
+                        <div class="col-2 ps-1 pe-3">
                         </div>
                     </div>
                     <div id="powerProduction">
@@ -33,19 +33,19 @@ $powerProduction = PowerProduction::getPowerProduction($gameSave->id);
                             <?php foreach ($powerProduction as $power) : ?>
                                 <div class="card mb-2" id="powerProductionCard<?= $power->id ?>">
                                     <div class="card-body p-2 row d-flex justify-content-between align-items-center">
-                                        <div class="col-6">
+                                        <div class="col-5 ps-3 pe-1 col-lg-6">
                                             <h6 class="m-0"><?= $power->building_name ?></h6>
                                         </div>
-                                        <div class="col-2">
+                                        <div class="col-2 px-1">
                                             <input type="number" class="form-control" id="amount" name="amount"
                                                    min="1" max="1000" value="<?= $power->amount ?>">
                                         </div>
-                                        <div class="col-2">
+                                        <div class="col-2 px-1">
                                             <input type="number" class="form-control" id="clock_speed"
                                                    name="clock_speed" min="1" max="250" step="any"
                                                    value="<?= $power->clock_speed ?>">
                                         </div>
-                                        <div class="col-2 text-end">
+                                        <div class="col-3 col-lg-2 text-end ps-1 pe-3">
                                             <button type="button" class="btn btn-danger deletePowerProduction"
                                                     data-id="<?= $power->building_id ?>">Delete
                                             </button>
@@ -62,7 +62,7 @@ $powerProduction = PowerProduction::getPowerProduction($gameSave->id);
                         <hr>
                         <div class="card mb-2" id="powerProductionCardNew">
                             <div class="card-body p-2 row ">
-                                <div class="col-6">
+                                <div class="col-5 col-lg-6 ps-3 pe-1">
                                     <select class="form-select" id="building" name="building">
                                         <option value="" disabled selected>Select a building</option>
                                         <?php foreach ($powerProductionBuildings as $building) : ?>
@@ -71,19 +71,19 @@ $powerProduction = PowerProduction::getPowerProduction($gameSave->id);
                                     </select>
                                     <div class="invalid-feedback">Please select a building</div>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-2  px-1">
                                     <input type="number" class="form-control" id="amount" name="amount" min="1"
                                            max="1000"
                                            value="1">
                                     <div class="invalid-feedback">Amount must be 1-1000</div>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-2 px-1">
                                     <input type="number" class="form-control" id="clock_speed" name="clock_speed"
                                            min="1"
                                            max="250" step="any" value="100">
                                     <div class="invalid-feedback">Clock speed must be 1-250</div>
                                 </div>
-                                <div class="col-2 text-end">
+                                <div class="col-3 col-lg-2 text-end px-1 pe-3">
                                     <button type="button" class="btn btn-primary" id="addPowerProduction">Add</button>
                                 </div>
                             </div>
