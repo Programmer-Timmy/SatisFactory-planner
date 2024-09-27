1ae87a9914dd20d7678148a50d87286948f349a8
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-09-27 22:50:10 +0200

    added class

---
+++ public/TypeScript/Tables/Classes/Visualization.ts
@@ -0,0 +1,13 @@
+import {TableHandler} from "./TableHandler";
+
+class Visualization {
+    // this call wil visualise the TableHandler class using
+
+    public TableHandler: TableHandler;
+
+    constructor(tableHandler: TableHandler) {
+        this.TableHandler = tableHandler;
+    }
+
+
+}
