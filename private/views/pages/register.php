0105ac699327899296f6cf5b559ac1569c74dfc2
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-09-28 00:54:46 +0200

    autocomplete added

--- private/views/pages/register.php
+++ private/views/pages/register.php
@@ -42,19 +42,23 @@ if ($_POST) {
                     <form method="post">
                         <div class="mb-3">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" name="username" required>
+                            <input type="text" class="form-control" id="username" name="username" required
+                                   autocomplete="username">
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
+                            <input type="email" class="form-control" id="email" name="email" required
+                                   autocomplete="email">
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
+                            <input type="password" class="form-control" id="password" name="password" required
+                                   autocomplete="new-password">
                         </div>
                         <div class="mb-3">
                             <label for="password2" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="password2" name="password2" required>
+                            <input type="password" class="form-control" id="password2" name="password2" required
+                                   autocomplete="off">
                         </div>
                         <button type="submit" class="btn btn-primary">Register</button>
                         <a href="/login" class="btn btn-secondary">Go back</a>
