3fa67e45b9dfd9422e76169e7593e5fcbeca9de6
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-10-15 21:27:43 +0200

    improved some text

--- private/controllers/Users.php
+++ private/controllers/Users.php
@@ -127,5 +127,15 @@ class Users
         return $html;
     }
 
+    public static function checkIfValidated($username) {
+        $user = Database::get("users", ['*'], [], ['username' => $username]);
+        if ($user) {
+            if ($user->verified == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
