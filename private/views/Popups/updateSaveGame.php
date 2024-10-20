48f2fecdc443262115807e301e6a30fd3f119cd9
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-10-20 23:51:29 +0200

    autocomplete off

--- private/views/Popups/updateSaveGame.php
+++ private/views/Popups/updateSaveGame.php
@@ -31,7 +31,8 @@ if (isset($_GET['dedicatedServerId'])) {
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form method="post" enctype="multipart/form-data" id="updateSaveGameForm_<?= $gameSave->id ?>">
+                <form method="post" enctype="multipart/form-data" id="updateSaveGameForm_<?= $gameSave->id ?>"
+                      autocomplete="off">
                     <input type="hidden" name="id" value="<?= $gameSave->id ?>">
 
                     <div class="mb-3">
