dddb963474751926c24d8f68dd79bbd0b49d537a
Programmer-Timmy <tim.vanderkloet@gmail.com>
2024-11-05 12:44:08 +0100

    limit to 10

--- private/controllers/ErrorHandeler.php
+++ private/controllers/ErrorHandeler.php
@@ -16,14 +16,22 @@ class ErrorHandeler {
 
     }
 
-    public static function getAll404Logs() {
+    public static function getAll404Logs($limit = 10) {
         // user can be null
-        return Database::query("SELECT users.username, error_404_logs.requested_url, error_404_logs.ip_address, error_404_logs.referrer_url, error_404_logs.error_timestamp FROM error_404_logs LEFT JOIN users ON error_404_logs.users_id = users.id");
+        return Database::query("SELECT users.username, error_404_logs.requested_url, error_404_logs.ip_address, error_404_logs.referrer_url, error_404_logs.error_timestamp FROM error_404_logs LEFT JOIN users ON error_404_logs.users_id = users.id order by error_timestamp desc limit $limit");
     }
 
-    public static function getAll403Logs() {
+    public static function getAll403Logs($limit = 10) {
         // user can be null
-        return Database::query("SELECT users.username, error_403_logs.requested_url, error_403_logs.ip_address, error_403_logs.referrer_url, error_403_logs.error_timestamp FROM error_403_logs LEFT JOIN users ON error_403_logs.users_id = users.id");
+        return Database::query("SELECT users.username, error_403_logs.requested_url, error_403_logs.ip_address, error_403_logs.referrer_url, error_403_logs.error_timestamp FROM error_403_logs LEFT JOIN users ON error_403_logs.users_id = users.id order by error_timestamp desc limit $limit");
+    }
+
+    public static function get404LogsByMonth($month, $year) {
+        return Database::query("SELECT users.username, error_404_logs.requested_url, error_404_logs.ip_address, error_404_logs.referrer_url, error_404_logs.error_timestamp FROM error_404_logs LEFT JOIN users ON error_404_logs.users_id = users.id WHERE MONTH(error_404_logs.error_timestamp) = ? AND YEAR(error_404_logs.error_timestamp) = ? order by error_timestamp desc", [$month, $year]);
+    }
+
+    public static function get403LogsByMonth($month, $year) {
+        return Database::query("SELECT users.username, error_403_logs.requested_url, error_403_logs.ip_address, error_403_logs.referrer_url, error_403_logs.error_timestamp FROM error_403_logs LEFT JOIN users ON error_403_logs.users_id = users.id WHERE MONTH(error_403_logs.error_timestamp) = ? AND YEAR(error_403_logs.error_timestamp) = ? order by error_timestamp desc", [$month, $year]);
     }
 
 }
