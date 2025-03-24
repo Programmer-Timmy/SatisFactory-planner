9b665804f6c4d8e7c24131483e1500577a09985c
Programmer-Timmy <tim.vanderkloet@gmail.com>
2025-03-24 09:42:59 +0100

    Enhance account page functionality: add user data deletion option, display last logins, and overview of saved data

--- private/views/pages/account/delete-data.php
+++ private/views/pages/account/delete-data.php
@@ -1,31 +1,43 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $userId = $_SESSION['user_id'];
+    $userId = $_SESSION['userId'];
 
-    // Verwijder alleen niet-essentiële data (geen account)
-    Database::delete(table: "login_attempts", where: ["user_id" => $userId]);
-    Database::delete(table: "error_404_logs", where: ["users_id" => $userId]);
-    Database::delete(table: "error_403_logs", where: ["users_id" => $userId]);
+    $database = new NewDatabase();
+    $database->beginTransaction();
+    try {
+
+        $database->delete(table: "login_attempts", where: ["users_id" => $userId]);
+        $database->delete(table: "error_404_logs", where: ["users_id" => $userId]);
+        $database->delete(table: "error_403_logs", where: ["users_id" => $userId]);
+
+        $database->commit();
+    } catch (Exception $e) {
+        $database->rollBack();
+        $_SESSION['error'] = "An error occurred while deleting your personal data. Please try again later.";
+        header('Location: /account/delete-data');
+    }
 
     $_SESSION['success'] = "Your personal data has been successfully deleted!";
+    header('Location: /account');
     exit;
 }
 ?>
 
-<div class="col-md-3"></div>
-<div class="col-md-6">
-    <h3 class="card-title text-center">Delete Personal Data</h3>
-</div>
-<div class="col-md-3"></div>
-<div class="card-text">
-    <div class="alert alert-danger text-center" role="alert">
-        <strong>Warning!</strong> This action cannot be undone. All your personal data will be deleted.
-    </div>
-    <form method="post">
-        <div class="d-flex justify-content-center">
-            <button type="submit" class="btn btn-danger">Delete Data</button>
-        </div>
-    </form>
-</div>
+<div class="container">
+    <h1 class="text-center text-danger mb-4">Delete Personal Data</h1>
 
+    <?php GlobalUtility::displayFlashMessages(); ?>
+
+    <p class="text-center fs-5">
+        Are you sure you want to delete your personal data? This action cannot be undone. Note that this will only delete non-essential data.
+    </p>
+
+    <div class="d-flex justify-content-center mt-4">
+        <form method="post">
+            <button type="submit" value="delete-personal-data" name="delete-personal-data" class="btn btn-danger me-3 px-4">Delete Data
+            </button>
+        </form>
+        <a href="/account" class="btn btn-primary me-3 px-4">Cancel</a>
+    </div>
+</div>
