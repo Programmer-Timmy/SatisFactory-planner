2285af6377ade6864f04d77ffd31a23616288225
Programmer-Timmy <tim.vanderkloet@gmail.com>
2025-04-10 11:42:12 +0200

    Add production line details to display import, production, power, and checklist counts

--- private/views/pages/admin/users/game-saves/production-lines.php
+++ private/views/pages/admin/users/game-saves/production-lines.php
@@ -17,12 +17,24 @@ if (!$gameSave && !Users::getUserById($userId)) {
 
 $productionLines = ProductionLines::getProductionLinesByGameSave($gameSaveId);
 
+foreach ($productionLines as $productionLine) {
+    $imports = ProductionLines::getImportsByProductionLine($productionLine->id);
+    $production = ProductionLines::getProductionByProductionLine($productionLine->id);
+    $powers = ProductionLines::getPowerByProductionLine($productionLine->id);
+    $checklist = Checklist::getChecklist($productionLine->id);
+
+    $productionLine->import_rows = count($imports);
+    $productionLine->production_rows = count($production);
+    $productionLine->power_rows = count($powers);
+    $productionLine->checks = count($checklist);
+
+}
 ?>
 <div class="container">
     <div class="row align-items-center mb-3">
         <div class="col-lg-2"></div>
         <div class="col-lg-8">
-            <h1 class="text-center">Production Lines for <b><?= htmlspecialchars($gameSave->title)?></b></h1>
+            <h1 class="text-center">Production Lines for <b><?= htmlspecialchars($gameSave->title) ?></b></h1>
             <p class="text-center">Created at: <?= htmlspecialchars($gameSave->created_at) ?></p>
         </div>
         <div class="col-lg-2 text-lg-end text-center">
@@ -31,14 +43,26 @@ $productionLines = ProductionLines::getProductionLinesByGameSave($gameSaveId);
     </div>
 
     <?= GlobalUtility::createTable(
-        $productionLines,
-        [
-            'name',
-            'power_consumbtion',
-            'updated_at',
-            'active'
-        ],
+                      $productionLines,
+                      [
+                          'name',
+                          'power_consumbtion',
+                          'import_rows',
+                          'production_rows',
+                          'power_rows',
+                          'checks',
+                          'updated_at',
+                          'active',
+                      ],
+        excludeBools: [
+                          'power_consumbtion',
+                          'import_rows',
+                          'production_rows',
+                          'power_rows',
+                          'checks',
+                      ]
     ) ?>
-    <p class="text-muted text-center">You can't edit, delete or add production lines here. Only the user can do that.</p>
+    <p class="text-muted text-center">You can't edit, delete or add production lines here. Only the user can do
+        that.</p>
 </div>
 
