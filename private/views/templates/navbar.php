2a96e19cee88123d43cf2408b2c7001942ff07b7
Programmer-Timmy <tim.vanderkloet@gmail.com>
2025-09-16 17:33:51 +0200

    Correct typo in navbar brand name from "Satisfactroy Planner" to "Satisfactory Planner"

--- private/views/templates/navbar.php
+++ private/views/templates/navbar.php
@@ -77,7 +77,7 @@ $navItems[] = new NavItem('/helpfulLinks', 'Helpful Links', $require === '/helpf
 </style>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/home">Satisfactroy Planner</a>
+        <a class="navbar-brand" href="/home">Satisfactory Planner</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
